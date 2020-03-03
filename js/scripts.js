aa4972fd288d44a8a17aec00b5f2e035d7d62d01
kidrey-david <60644872+kidrey-david@users.noreply.github.com>
2020-03-03 17:03:58 +0300

    added js and bootstrap (#2)
    * added images
    
    * added js and bootstrap

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+$("#submit").click(function(){
+    var name = $("#contactName").val();
+    var email = $("#contactEmail").val();
+    var info = $("#textArea").val();
+    $(".contactName").text(name);
+    $(".contactEmail").text(email);
+    $(".textArea").text(info);
+    alert("Thank you " + name + " for your feedback. We will be in touch via " +email);
+    });
+});  
