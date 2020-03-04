8afe30f41503b1bcea736e2958da3a73d4bf5676
DanielGithiomi <danielgithiomi@gmail.com>
2020-03-04 22:45:09 +0300

    male without functioning js (#8)
    * This is the males.html page with active overlays
    
    * styled and resized overlays
    
    * kidrey with a few non-functional js
    
    Co-authored-by: DanielGithiomi <danielgithiomi@gmail.com>

---
+++ js/males.js
@@ -0,0 +1,13 @@
+// $(document).ready(function() {
+//     $(".clickable").click(function() {
+//       $(".afro-curly-showing").toggle();
+//       $(".afro-curly-hidden").toggle();
+//       $("#afro-curly").show();
+//     });
+// });  
+
+function placeOrder() {
+  var price = document.getElementById("ordertype").value;
+
+  console.log(price);
+}
