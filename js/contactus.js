74c9a4611ef8851a0661b130400c0179376cee02
Naftaly Muiruri <muirurigathairu@gmail.com>
2020-03-04 16:19:57 -0500

    adds social media icon

---
+++ js/contactus.js
@@ -0,0 +1,38 @@
+$(function(){
+
+    $("#q1").hover(function(){
+        $("#q1-q").show()
+        $("#q1-ans").show(1300)       
+    });
+
+    $("#q2").hover(function(){
+        $("#q2-q").show() 
+        $("#q2-ans").show(1300)       
+    });
+
+    $("#q3").hover(function(){
+        $("#q3-q").show() 
+        $("#q3-ans").show(1300)       
+    });
+
+    $("#q4").hover(function(){
+        $("#q4-q").show() 
+        $("#q4-ans").show(1300)       
+    });
+
+    $("#q5").hover(function(){
+        $("#q5-q").show() 
+        $("#q5-ans").show(1300)       
+    });
+
+    $("#q6").hover(function(){
+        $("#q6-q").show() 
+        $("#q6-ans").show(1300)       
+    });
+
+    $("#q7").hover(function(){
+        $("#q7-q").show() 
+        $("#q7-ans").show(1300)       
+    });
+
+});
