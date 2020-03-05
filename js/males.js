3cce8511ef87cefab1cf6f3c5279f257c78b8661
david <neymardavid21@gmail.com>
2020-03-05 11:56:51 +0300

    done with male page for real this time

--- js/males.js
+++ js/males.js
@@ -11,55 +11,55 @@ var plusTrans = 0;
 function placeOrder1() {
   price = parseInt(document.getElementById('overlay1').getAttribute('value'));
 
-  $("#appear").show(750);
+  $("#appear").show();
   $(".haircut").text("You have selected the Mohawk hairstyle that will cost you Ksh: " + price);
 }
 function placeOrder2() {
   price = parseInt(document.getElementById('overlay2').getAttribute('value'));
 
-  $("#appear").show(750);
+  $("#appear").show();
   $(".haircut").text("You have selected the Dreads Fade Long hairstyle that will cost you Ksh: " + price);
 }
 function placeOrder3() {
   price = parseInt(document.getElementById('overlay3').getAttribute('value'));
 
-  $("#appear").show(750);
+  $("#appear").show();
   $(".haircut").text("You have selected the Dreads Fade Top hairstyle that will cost you Ksh: " + price);
 }
 function placeOrder4() {
   price = parseInt(document.getElementById('overlay4').getAttribute('value'));
 
-  $("#appear").show(750);
+  $("#appear").show();
   $(".haircut").text("You have selected the Dyed Fade hairstyle that will cost you Ksh: " + price);
 }
 function placeOrder5() {
   price = parseInt(document.getElementById('overlay5').getAttribute('value'));
 
-  $("#appear").show(750);
+  $("#appear").show();
   $(".haircut").text("You have selected the Fade hairstyle that will cost you Ksh: " + price);
 }
 function placeOrder6() {
   price = parseInt(document.getElementById('overlay6').getAttribute('value'));
 
-  $("#appear").show(750);
+  $("#appear").show();
   $(".haircut").text("You have selected the Fade X Cuts hairstyle that will cost you Ksh: " + price);
 }
 function placeOrder7() {
   price = parseInt(document.getElementById('overlay7').getAttribute('value'));
 
-  $("#appear").show(750);
+  $("#appear").show();
   $(".haircut").text("You have selected the Braids hairstyle that will cost you Ksh: " + price);
 }
 function placeOrder8() {
   price = parseInt(document.getElementById('overlay8').getAttribute('value'));
 
-  $("#appear").show(750);
+  $("#appear").show();
   $(".haircut").text("You have selected the Taper Fade hairstyle that will cost you Ksh: " + price);
 }
 function placeOrder9() {
   price = parseInt(document.getElementById('overlay9').getAttribute('value'));
 
-  $("#appear").show(750);
+  $("#appear").show();
   $(".haircut").text("You have selected the Afro Curly hairstyle that will cost you Ksh: " + price);
 }
 function detailsShow() {
