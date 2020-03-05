149840ce7f9fca18a15cf9537bae5abb9b553054
angiewambui <wambuinjenga44@gmail.com>
2020-03-05 11:14:25 -0500

    added female.js

---
+++ js/females.js
@@ -0,0 +1,62 @@
+// $(document).ready(function() {
+//     $(".clickable").click(function() {
+//       $(".afro-curly-showing").toggle();
+//       $(".afro-curly-hidden").toggle();
+//       $("#afro-curly").show();
+//     });
+// });  
+
+function placeOrder1() {
+    var price = parseInt(document.getElementById('overlay1').getAttribute('value'));
+  
+    $("#appear").show(1350);
+    $(".haircut").text("You have selected the Short Side  hairstyle that will cost you Ksh: " + price);
+  }
+  function placeOrder2() {
+    var price = parseInt(document.getElementById('overlay2').getAttribute('value'));
+  
+    $("#appear").show(2500);
+    $(".haircut").text("You have selected the Kinky hairstyle that will cost you Ksh: " + price);
+  }
+  function placeOrder3() {
+    var price = parseInt(document.getElementById('overlay3').getAttribute('value'));
+  
+    $("#appear").show(1000);
+    $(".haircut").text("You have selected the Blonde short hairstyle that will cost you Ksh: " + price);
+  }
+  function placeOrder4() {
+    var price = parseInt(document.getElementById('overlay4').getAttribute('value'));
+  
+    $("#appear").show(800);
+    $(".haircut").text("You have selected the Curly long hairstyle that will cost you Ksh: " + price);
+  }
+  function placeOrder5() {
+    var price = parseInt(document.getElementById('overlay5').getAttribute('value'));
+  
+    $("#appear").show(1350);
+    $(".haircut").text("You have selected the Curly  hairstyle that will cost you Ksh: " + price);
+  }
+  function placeOrder6() {
+    var price = parseInt(document.getElementById('overlay6').getAttribute('value'));
+  
+    $("#appear").show(1600);
+    $(".haircut").text("You have selected the Dreads hairstyle that will cost you Ksh: " + price);
+  }
+  function placeOrder7() {
+    var price = parseInt(document.getElementById('overlay7').getAttribute('value'));
+  
+    $("#appear").show(1300);
+    $(".haircut").text("You have selected the Braids hairstyle that will cost you Ksh: " + price);
+  }
+  function placeOrder8() {
+    var price = parseInt(document.getElementById('overlay8').getAttribute('value'));
+  
+    $("#appear").show(3500);
+    $(".fhaircut").text("You have selected the Short Fade hairstyle that will cost you Ksh: " + price);
+  }
+  function placeOrder9() {
+    var price = parseInt(document.getElementById('overlay9').getAttribute('value'));
+  
+    $("#appear").show(2000);
+    $(".haircut").text("You have selected the Afro Curly hairstyle that will cost you Ksh: " + price);
+  }
