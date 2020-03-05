46a920cebd3443f68ab4aa1d34f7c89829340c1d
DanielGithiomi <danielgithiomi@gmail.com>
2020-03-05 12:06:12 +0300

    Feature/kidrey (#14)
    * styled and resized overlays
    
    * kidrey with a few non-functional js
    
    * Male page completed fully including JS
    
    * done with male page
    
    * done with male page for real this time

--- js/males.js
+++ js/males.js
@@ -5,58 +5,109 @@
 //       $("#afro-curly").show();
 //     });
 // });  
+var price = 0;
+var plusTrans = 0;
 
 function placeOrder1() {
-  var price = parseInt(document.getElementById('overlay1').getAttribute('value'));
+  price = parseInt(document.getElementById('overlay1').getAttribute('value'));
 
-  $("#appear").show(750);
+  $("#appear").show();
   $(".haircut").text("You have selected the Mohawk hairstyle that will cost you Ksh: " + price);
 }
 function placeOrder2() {
-  var price = parseInt(document.getElementById('overlay2').getAttribute('value'));
+  price = parseInt(document.getElementById('overlay2').getAttribute('value'));
 
-  $("#appear").show(750);
+  $("#appear").show();
   $(".haircut").text("You have selected the Dreads Fade Long hairstyle that will cost you Ksh: " + price);
 }
 function placeOrder3() {
-  var price = parseInt(document.getElementById('overlay3').getAttribute('value'));
+  price = parseInt(document.getElementById('overlay3').getAttribute('value'));
 
-  $("#appear").show(750);
+  $("#appear").show();
   $(".haircut").text("You have selected the Dreads Fade Top hairstyle that will cost you Ksh: " + price);
 }
 function placeOrder4() {
-  var price = parseInt(document.getElementById('overlay4').getAttribute('value'));
+  price = parseInt(document.getElementById('overlay4').getAttribute('value'));
 
-  $("#appear").show(750);
+  $("#appear").show();
   $(".haircut").text("You have selected the Dyed Fade hairstyle that will cost you Ksh: " + price);
 }
 function placeOrder5() {
-  var price = parseInt(document.getElementById('overlay5').getAttribute('value'));
+  price = parseInt(document.getElementById('overlay5').getAttribute('value'));
 
-  $("#appear").show(750);
+  $("#appear").show();
   $(".haircut").text("You have selected the Fade hairstyle that will cost you Ksh: " + price);
 }
 function placeOrder6() {
-  var price = parseInt(document.getElementById('overlay6').getAttribute('value'));
+  price = parseInt(document.getElementById('overlay6').getAttribute('value'));
 
-  $("#appear").show(750);
+  $("#appear").show();
   $(".haircut").text("You have selected the Fade X Cuts hairstyle that will cost you Ksh: " + price);
 }
 function placeOrder7() {
-  var price = parseInt(document.getElementById('overlay7').getAttribute('value'));
+  price = parseInt(document.getElementById('overlay7').getAttribute('value'));
 
-  $("#appear").show(750);
+  $("#appear").show();
   $(".haircut").text("You have selected the Braids hairstyle that will cost you Ksh: " + price);
 }
 function placeOrder8() {
-  var price = parseInt(document.getElementById('overlay8').getAttribute('value'));
+  price = parseInt(document.getElementById('overlay8').getAttribute('value'));
 
-  $("#appear").show(750);
+  $("#appear").show();
   $(".haircut").text("You have selected the Taper Fade hairstyle that will cost you Ksh: " + price);
 }
 function placeOrder9() {
-  var price = parseInt(document.getElementById('overlay9').getAttribute('value'));
+  price = parseInt(document.getElementById('overlay9').getAttribute('value'));
 
-  $("#appear").show(750);
+  $("#appear").show();
   $(".haircut").text("You have selected the Afro Curly hairstyle that will cost you Ksh: " + price);
+}
+function detailsShow() {
+  $(".info").slideToggle(1500);
+}
+function displayDelivery() {
+  $(".addressInput").show(1000);
+  $(".noDelivery").hide(1000);
+}
+function hideDelivery() {
+  $(".addressInput").hide(1000);
+  $(".noDelivery").slideToggle(1000);
+}
+
+function Pdetails(fname, lname, number, email, cname,sname){
+  this.fname = fname;
+  this.lname = lname;
+  this.number = number;
+  this.email = email;
+  this.cname = cname;
+  this.sname = sname;
+}
+
+function confirmDetails(){
+     var firstName = $("#fName").val();
+     var lastName = $("#lName").val();
+     var phoneNumber = $("#pNumber").val();
+     var emailAddress = $("#eAddress").val();
+     var cityName = $("#cName").val();
+     var streetName = $("#sName").val();
+   
+     var Person = new  Pdetails (firstName, lastName, phoneNumber, emailAddress, cityName, streetName);
+
+     plusTrans = price + 150;
+  
+  Pdetails.prototype.fullName = function() {
+     return Person.fname + " " + Person.lname;
+  }
+  Pdetails.prototype.contactDetails = function() {
+     return Person.number + ", "  + Person.email;
+  }
+  Pdetails.prototype.deliveryAddress = function() {
+     return Person.cname + ", " + Person.sname;
+  }
+
+      $("#showDetails").slideToggle();
+      $(".fullName").text(Person.fullName);
+      $(".contactInfo").text(Person.contactDetails);
+      $(".orderLocation").text(Person.deliveryAddress);
+      $(".plusTrans").text(plusTrans);
 }
